f26a970de8f04264a163407e6b5fbec4e31c253b
Zach Oda <zjodachow@gmail.com>
2022-08-23 13:34:57 -0400

    updated routes

---
+++ Just Tech News/config/connection.js
@@ -0,0 +1,13 @@
+//import the Sequelize constructor from the library
+const Sequelize = require("sequelize");
+
+require("dotenv").config();
+
+//create connection to our database using mySQL info
+const sequelize = new Sequelize(process.env.DB_Name, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: "localhost",
+    dialect: "mysql",
+    port: 3306
+});
+
+module.exports = sequelize;
